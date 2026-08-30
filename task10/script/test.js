6fe690664f06a43f71f5af43a2d53d2ea97ad628
yassinhaythamr <yassinhaytham242@gmail.com>
2026-08-30 10:37:32 +0300

    task10

---
+++ task10/script/test.js
@@ -0,0 +1,25 @@
+// cancel , ok (empty string) ==> alert 
+// > 100 ==> invalid degree
+// > 90  ==> Excellent
+// > 80 ==> very good
+// > 70 ==> good
+// < 70 ==> failed
+var degree = prompt("enter your degree")
+if(degree==null || degree=="" ){
+    alert('please enter your degree')
+}
+else if(degree>100){
+    alert('invalid degree')
+}
+else if(degree>90){
+    alert('Excellent')
+}
+else if(degree>80){
+    alert('very good')
+}else if(degree>70){
+    alert('good')
+}
+else {
+    alert('fail')
+}
+
